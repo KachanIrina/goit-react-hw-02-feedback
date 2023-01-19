ed14369e35839bf72c29bc67b3f6ad46e9b6cab7
KachanIrina <106675851+KachanIrina@users.noreply.github.com>
2023-01-19 22:04:34 +0200

    Завершена робота

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,66 @@
+import React from 'react';
 import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
+import { Notification } from './Notification/Notification';
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        // justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-        paddingLeft: 30,
-      }}
-    >
-      <Statistics
-        initialGood={0}
-        initialNeutral={0}
-        initialBad={0}
-        total={0}
-        positivePercentage={0}
-      />
-    </div>
-  );
-};
+export class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  hendleIncrement = option => {
+    this.setState(prevState => ({
+      [option]: prevState[option] + 1,
+    }));
+  };
+
+  total = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  feedbackPercentage = () => {
+    if (!this.total()) {
+      return 0;
+    }
+    return Math.round((this.state.good / this.total()) * 100);
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          //display: 'flex',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+          paddingLeft: 30,
+        }}
+      >
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.hendleIncrement}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          {this.total() !== 0 ? (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.total()}
+              positivePercentage={this.feedbackPercentage()}
+            />
+          ) : (
+            <Notification message="There is no feedback"></Notification>
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,67 +1,26 @@
-import FeedbackOptions from './FeedbackOptions';
 import css from './Statistics.module.css';
-import React from 'react';
-import PropTypes from 'prop-types';
+import propTypes from 'prop-types';
 
-export class Statistics extends React.Component {
-  static defaultProps = {
-    initialGood: 0,
-    initialNeutral: 0,
-    initialBad: 0,
-    total: 0,
-    feedback: 0,
-  };
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => (
+  <div className={css.stats}>
+    <span>Good: {good}</span>
+    <span>Neutral: {neutral}</span>
+    <span>Bad: {bad}</span>
+    <span>Total: {total}</span>
+    <span>Positive feedback: {positivePercentage}%</span>
+  </div>
+);
 
-  static propTypes = {
-    initialGood: PropTypes.number.isRequired,
-    initialNeutral: PropTypes.number.isRequired,
-    initialBad: PropTypes.number.isRequired,
-  };
-
-  state = {
-    good: this.props.initialGood,
-    neutral: this.props.initialNeutral,
-    bad: this.props.initialBad,
-  };
-
-  hendleIncrementGood = good => {
-    this.setState(prevState => ({
-      good: prevState.good + 1,
-    }));
-  };
-
-  hendleIncrementNeutral = neutral => {
-    this.setState(prevState => ({
-      neutral: prevState.neutral + 1,
-    }));
-  };
-
-  hendleIncrementBad = neutral => {
-    this.setState(prevState => ({
-      bad: prevState.bad + 1,
-    }));
-  };
-
-  render() {
-    return (
-      <div>
-        <h1 className={css.title}>Please leave feedback</h1>
-
-        <FeedbackOptions
-          onIncrementGood={this.hendleIncrementGood}
-          onIncrementNeutral={this.hendleIncrementNeutral}
-          onIncrementBad={this.hendleIncrementBad}
-        />
-
-        <div className={css.stats}>
-          <h2 className={css.title}>Statistics</h2>
-          <span>Good: {this.state.good}</span>
-          <span>Neunral: {this.state.neutral}</span>
-          <span>Bad: {this.state.bad}</span>
-          <span>Total:</span>
-          <span>Positive feedback:</span>
-        </div>
-      </div>
-    );
-  }
-}
+Statistics.propTypes = {
+  good: propTypes.number.isRequired,
+  neutral: propTypes.number.isRequired,
+  bad: propTypes.number.isRequired,
+  total: propTypes.number.isRequired,
+  positivePercentage: propTypes.number.isRequired,
+};
